e002f21f881fa0251910c8877914fd319576ee50
Lukas Holecek <hluk@email.cz>
2021-12-01 20:36:23 +0100

    Clean up solution for day 1

--- 01/main.nim
+++ 01/main.nim
@@ -2,10 +2,9 @@ import sequtils
 import strutils
 import math
 
-let contents = readFile("input").strip()
-let ns = contents.splitLines().map(parseInt)
+let ns = "input".lines.toSeq.map(parseInt)
 
-echo "Part1: ", zip(ns[0..^2], ns[1..^1]).countIt(it[0] < it[1])
-echo "Part2: ", toSeq(3 ..< len(ns)).countIt(
+echo "Part1: ", zip(ns, ns[1..^1]).countIt(it[0] < it[1])
+echo "Part2: ", countIt(3 ..< len(ns),
   sum(ns[it - 3 .. it - 1]) < sum(ns[it - 2 .. it])
 )
