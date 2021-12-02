979f0604cb3f7657f01c3921d9c2dc6e6f9af32d
Lukas Holecek <hluk@email.cz>
2021-12-02 07:37:21 +0100

    Use named tuples

--- 02/main.nim
+++ 02/main.nim
@@ -3,19 +3,19 @@ import strutils
 
 var lines = "input".lines.toSeq
   .mapIt(it.split)
-  .mapIt((it[0], it[1].parseInt))
+  .mapIt((dir: it[0], x: it[1].parseInt))
 
-var horizontal = lines.foldl(if b[0] == "forward": a + b[1] else: a, 0)
+var horizontal = lines.foldl(if b.dir == "forward": a + b.x else: a, 0)
 var depth = lines.foldl(
-  if b[0] == "down": a + b[1]
-  elif b[0] == "up": a - b[1]
+  if b.dir == "down": a + b.x
+  elif b.dir == "up": a - b.x
   else: a, 0
 )
 echo "Part1: ", horizontal * depth
 
 var depth_and_aim = lines.foldl(
-  if b[0] == "down": (a[0], a[1] + b[1])
-  elif b[0] == "up": (a[0], a[1] - b[1])
-  else: (a[0] + a[1] * b[1], a[1]),
-  (0, 0))
-echo "Part2: ", horizontal * depth_and_aim[0]
+  if b.dir == "down": (a.depth, a.aim + b.x)
+  elif b.dir == "up": (a.depth, a.aim - b.x)
+  else: (a.depth + a.aim * b.x, a.aim),
+  (depth: 0, aim: 0))
+echo "Part2: ", horizontal * depth_and_aim.depth
