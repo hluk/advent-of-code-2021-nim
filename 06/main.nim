71fd679a73d33b9486ac3ec38e3374c6fd045511
Lukas Holecek <hluk@email.cz>
2021-12-06 16:41:30 +0100

    Remove raw for loop

--- 06/main.nim
+++ 06/main.nim
@@ -2,13 +2,13 @@ import math
 import sequtils
 import strutils
 
-type Timers = array[0..8, int]
+type Timers = array[9, int]
 
 proc run*(timers: Timers, days: int): int =
-  var ts = timers
-  for i in 0..<days:
-    ts[(i + 7) mod 9] += ts[i mod 9]
-  ts.sum
+  toSeq(0..<days).foldl(
+    [a[1], a[2], a[3], a[4], a[5], a[6], a[7] + a[0], a[8], a[0]],
+    timers
+  ).sum
 
 proc parseTimers*(filename: string): Timers =
   let timers = filename.readFile.strip.split(",")
