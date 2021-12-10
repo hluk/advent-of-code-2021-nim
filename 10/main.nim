49aba19a0b588fa313ef90741b2450a6691b71af
Lukas Holecek <hluk@email.cz>
2021-12-10 07:48:40 +0100

    Refactor day 10

--- 10/main.nim
+++ 10/main.nim
@@ -1,63 +1,43 @@
 import algorithm
-import math
-import sequtils
-import tables
+import zero_functional
 
-proc pair(c: char): (int, int) =
-    case c
-    of '(': (1, 1)
-    of ')': (1, -1)
-    of '[': (2, 1)
-    of ']': (2, -1)
-    of '{': (3, 1)
-    of '}': (3, -1)
-    of '<': (4, 1)
-    of '>': (4, -1)
-    else: raise newException(ValueError, "Unexpected character: " & c)
+const PAIRS = "()[]{}<>"
 
-proc score1(c: char): int =
-    case c
-    of ')': 3
-    of ']': 57
-    of '}': 1197
-    of '>': 25137
-    else: raise newException(ValueError, "Unexpected character: " & c)
+proc value(c: char): int =
+  PAIRS.find(c) div 2 + 1
 
-proc part1*(filename: string): int =
-  for line in filename.lines:
-    var braces: seq[int]
-    for c in line:
-      let (i, d) = pair(c)
-      if d == 1:
-        braces.add(i)
-      elif d == -1 and braces[^1] == i:
-        discard braces.pop
-      else:
-        result += score1(c)
-        break
+proc isOpening(c: char): bool =
+  PAIRS.find(c) mod 2 == 0
+
+proc score(c: char): int =
+  case c
+  of ')': 3
+  of ']': 57
+  of '}': 1197
+  of '>': 25137
+  else: raise newException(ValueError, "Unexpected character: " & c)
+
+proc unclosed(line: string, i: int = 0, expect: seq[int] = newSeq[int]()): (int, int) =
+    if i >= line.len:
+      return (0, expect.reversed --> fold(0, a * 5 + it))
+
+    if isOpening(line[i]):
+      unclosed(line, i + 1, expect & value(line[i]))
+    elif expect[^1] == value(line[i]):
+      unclosed(line, i + 1, expect[0..^2])
+    else:
+      (score(line[i]), 0)
+
+proc solve*(filename: string): (int, int) =
+  var scores = filename.lines --> map(unclosed)
+  let part1 = scores --> fold(0, a + it[0])
+  let part2 = scores --> map(it[1]) --> filter(it != 0) --> sorted
+  (part1, part2[part2.len div 2])
 
 proc part2*(filename: string): int =
-  var scores: seq[int]
-  for line in filename.lines:
-    var braces: seq[int]
-    for c in line:
-      let (i, d) = pair(c)
-      if d == 1:
-        braces.add(i)
-      elif d == -1 and braces[^1] == i:
-        discard braces.pop
-      else:
-        braces.setLen(0)
-        break
-    if braces.len > 0:
-      var score = 0
-      for brace in braces.reversed:
-        score = score * 5 + brace
-      scores.add(score)
-
+  var scores = filename.lines --> map(unclosed(it)[1]).filter(it != 0)
   scores.sort
   scores[scores.len div 2]
 
 if isMainModule:
-  echo "Part1: ", "input".part1
-  echo "Part2: ", "input".part2
+  echo "input".solve
--- 10/test.nim
+++ 10/test.nim
@@ -3,7 +3,7 @@ import unittest
 
 suite "day 10":
   test "part1":
-    check "test1".part1 == 26397
+    check "test1".solve[0] == 26397
 
   test "part2":
-    check "test1".part2 == 288957
+    check "test1".solve[1] == 288957
