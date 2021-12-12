d1ca697316a9396e6fa5d2b2ed3f06cf97bf3a13
Lukas Holecek <hluk@email.cz>
2021-12-12 09:55:22 +0100

    Improve performance by using uint8 instead of string

--- 12/main.nim
+++ 12/main.nim
@@ -1,36 +1,48 @@
 import strutils
 import tables
 
-type Paths = Table[string, seq[string]]
-type Visited = seq[string]
+type Path = uint8
+type Paths = Table[Path, seq[Path]]
+type Visited = seq[Path]
+
+const START: Path = 0
+const END: Path = 1
+const FIRST_LARGE_CAVE_ID: Path = 100
 
 proc parsePaths*(filename: string): Paths =
+  var caveIds: Table[string, Path]
+  var availableId: Path = 2
+  caveIds["start"] = START
+  caveIds["end"] = END
+
   for line in filename.lines:
-    let path = line.split("-")
-    result.mgetOrPut(path[0], @[]).add(path[1])
-    result.mgetOrPut(path[1], @[]).add(path[0])
+    let path = line.split("-", 1)
+    let ax: Path = if path[0][0].isUpperAscii: 100 else: 0
+    let bx: Path = if path[1][0].isUpperAscii: 100 else: 0
+    let a = caveIds.mgetOrPut(path[0], availableId + ax)
+    let b = caveIds.mgetOrPut(path[1], availableId + 1 + bx)
+    result.mgetOrPut(a, @[]).add(b)
+    result.mgetOrPut(b, @[]).add(a)
+    availableId += 2
 
-proc canVisitAgain(cave: string): bool =
-  cave[0].isUpperAscii
-
-proc visit(p: Paths, c: string, visited: var Visited, visitedTwice: bool): int =
+proc visit(p: Paths, c: Path, visited: var Visited, visitedTwice: bool): int =
   visited.add(c)
   for s in p.getOrDefault(c):
-    if s == "end":
+    if s == END:
       result += 1
-    elif s.canVisitAgain or not visited.contains(s):
+    elif s >= FIRST_LARGE_CAVE_ID or not visited.contains(s):
       result += p.visit(s, visited, visitedTwice)
-    elif s != "start" and not visitedTwice:
+    elif s != START and not visitedTwice:
       result += p.visit(s, visited, true)
   discard visited.pop()
 
 proc paths*(p: Paths): int =
   var visited: Visited
-  p.visit("start", visited, true)
+  p.visit(START, visited, true)
 
 proc paths2*(p: Paths): int =
   var visited: Visited
-  p.visit("start", visited, false)
+  p.visit(START, visited, false)
 
 if isMainModule:
   var p = "input".parsePaths
