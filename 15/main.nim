fe2cb0b6da3930820ad06c07b4380dedd7d3a22e
Lukas Holecek <hluk@email.cz>
2021-12-15 12:26:18 +0100

    Refactor day 15

--- 15/main.nim
+++ 15/main.nim
@@ -1,69 +1,50 @@
-import deques
 import sequtils
 
-type Map = seq[seq[int]]
+type Map = seq[seq[uint8]]
 type Pos = tuple[x: int, y: int]
 type Risks = seq[seq[int]]
 
 const INVALID = 999999
 
-proc `[]`(map: Map, pos: Pos): int =
+proc `[]`(map: Risks, pos: Pos): int =
   if 0 <= pos.y and pos.y < map.len and 0 <= pos.x and pos.x < map[pos.y].len:
     map[pos.y][pos.x]
   else:
     INVALID
 
-proc `[]=`(map: var Map, pos: Pos, value: int): void =
-  map[pos.y][pos.x] = value
-
 proc parseMap*(filename: string): Map =
-  filename.lines.toSeq.mapIt(it.toSeq.mapIt(it.ord - '0'.ord))
-
-iterator mapPositions(map: Map): Pos =
-  var positions: Deque[Pos]
-  let y = map.len - 1
-  let x = map[y].len - 1
-  positions.addLast((x, y))
-  while positions.len != 0:
-    let pos = positions.popFirst()
-    if pos.x > 0 and not positions.contains((pos.x-1, pos.y)):
-      positions.addLast((pos.x-1, pos.y))
-    if pos.y > 0 and not positions.contains((pos.x, pos.y-1)):
-      positions.addLast((pos.x, pos.y-1))
-    yield pos
+  filename.lines.toSeq.mapIt:
+    it.toSeq.mapIt:
+      (it.ord - '0'.ord).uint8
 
 proc risks*(map: Map): Risks =
-  result = newSeqWith(map.len, newSeq[int](map[0].len))
+  result = newSeqWith(map.len, newSeqWith[int](map[0].len, INVALID))
 
-  for pos in map.mapPositions:
-    let risk = map[pos]
-    let m = min(result[(pos.x,pos.y+1)], result[(pos.x+1,pos.y)])
-    if m == INVALID:
-      result[pos] = risk
-    else:
-      result[pos] = risk + m
+  let y0 = map.len - 1
+  let x0 = map[y0].len - 1
+  result[y0][x0] = map[y0][x0].int
 
-  for i in 0..1:
-    for pos in map.mapPositions:
-      let risk = map[pos]
-      result[pos] = min([
-        result[pos],
-        risk + result[(pos.x,pos.y-1)],
-        risk + result[(pos.x-1,pos.y)],
-        risk + result[(pos.x,pos.y+1)],
-        risk + result[(pos.x+1,pos.y)]
-      ])
+  for i in 0..2:
+    for y in countdown(y0, 0):
+      for x in countdown(x0, 0):
+        let m = [
+          result[(x, y-1)],
+          result[(x-1, y)],
+          result[(x, y+1)],
+          result[(x+1, y)]
+        ].min + map[y][x].int
+        result[y][x] = min(result[y][x], m)
 
 proc lowest*(map: Map): int =
-  map.risks[0][0] - map[(0,0)]
+  map.risks[0][0] - map[0][0].int
 
 proc enlarge*(tile: Map, rep: int = 5): Map =
-  result = newSeqWith(tile.len * rep, newSeq[int](tile[0].len * rep))
+  result = newSeqWith(tile.len * rep, newSeq[uint8](tile[0].len * rep))
   for (y, row) in result.mpairs:
     for (x, v) in row.mpairs:
       let u = tile[y mod tile.len][x mod tile[0].len]
       let d = (x div tile[0].len) + (y div tile.len)
-      v = ((u + d - 1) mod 9) + 1
+      v = ((u + d.uint8 - 1) mod 9) + 1
 
 proc lowest2*(tile: Map): int =
   tile.enlarge.lowest
