59d56c7dc107cbbf70cbf845bec98f982ea5fbb2
Lukas Holecek <hluk@email.cz>
2021-12-17 12:27:29 +0100

    Skip more iterations for day 17

--- 17/main.nim
+++ 17/main.nim
@@ -1,3 +1,4 @@
+import math
 import sequtils
 import strscans
 
@@ -11,25 +12,35 @@ proc parseTarget*(filename: string): Area =
   (x1, x2, y1, y2)
 
 proc reach(target: Area, v0: Velocity): (bool, int) =
-  var v = v0
-  var x, y: int
+  # Calculate initial time when X is in the target range.
+  let b: float = v0.x.float - 0.5
+  let c: float = v0.x.float - target.x1.float
+  let txx = b - sqrt(b^2 + 2*c)
+  let tx = txx.ceil.int
+
+  var y = (2 * v0.y - tx) * (tx + 1) div 2
+  var x = (2 * v0.x - tx) * (tx + 1) div 2
+  var v = (x: v0.x - tx, y: v0.y - tx)
+  assert x >= target.x1
 
   while true:
-    x += v.x
-    y += v.y
+    if x > target.x2 or y < target.y1:
+      return (false, 0)
 
-    if target.x1 <= x and x <= target.x2 and target.y1 <= y and y <= target.y2:
+    if y <= target.y2:
       let maxY = v0.y * (v0.y + 1) div 2
       return (true, maxY)
-
-    if x > target.x2 or y <= target.y1 or (v.x <= 0 and x < target.x1):
-      return (false, 0)
 
     if v.x > 0: dec v.x
     dec v.y
 
+    x += v.x
+    y += v.y
+
 iterator velocities(target: Area): Velocity =
-  for x in 1..target.x2:
+  # The first x velocity time that can reach the target.
+  let minX = sqrt(0.25 + 2 * target.x1.float) - 0.5
+  for x in minX.ceil.int..target.x2:
     for y in countdown(100, target.y1):
       yield (x, y)
 
