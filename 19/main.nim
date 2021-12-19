6b8c66f164af5290e700f905829fe870d79d520f
Lukas Holecek <hluk@email.cz>
2021-12-19 16:03:00 +0100

    Skip more iterations for day 19

--- 19/main.nim
+++ 19/main.nim
@@ -1,4 +1,3 @@
-import algorithm
 import math
 import sequtils
 import sets
@@ -6,21 +5,47 @@ import strscans
 import strutils
 import tables
 
-type Pos = tuple[x, y, z: int]
-type Rot = tuple[x, y, z: int]
+type Pos = array[3, int]
+type Rot = Pos
 type Scanner = seq[Pos]
 type Scanners = seq[Scanner]
 
 const MIN_BEACON_OVERLAP = 12
+const ROTATIONS: array[24, Rot] = [
+  [1, 2, 3],
+  [1,-3, 2],
+  [1,-2,-3],
+  [1, 3,-2],
+  [2, 3, 1],
+  [2,-1, 3],
+  [2,-3,-1],
+  [2, 1,-3],
+  [3, 1, 2],
+  [3,-2, 1],
+  [3,-1,-2],
+  [3, 2,-1],
+  [-3,-2,-1],
+  [-3, 1,-2],
+  [-3, 2, 1],
+  [-3,-1, 2],
+  [-2,-1,-3],
+  [-2, 3,-1],
+  [-2, 1, 3],
+  [-2,-3, 1],
+  [-1,-3,-2],
+  [-1, 2,-3],
+  [-1, 3, 2],
+  [-1,-2, 3],
+]
 
 proc `-`*(a: Pos): Pos =
-  (-a[0], -a[1], -a[2])
+  [-a[0], -a[1], -a[2]]
 
 proc `-`*(a: Pos, b: Pos): Pos =
-  (a[0] - b[0], a[1] - b[1], a[2] - b[2])
+  [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
 
 proc `+`*(a: Pos, b: Pos): Pos =
-  (a[0] + b[0], a[1] + b[1], a[2] + b[2])
+  [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
 
 proc manhattan*(a: Pos, b: Pos): int =
   abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
@@ -28,7 +53,7 @@ proc manhattan*(a: Pos, b: Pos): int =
 proc parsePos*(posString: string): Pos =
   let (ok, x, y, z) = posString.scanTuple("$i,$i,$i")
   assert ok
-  (x, y, z)
+  [x, y, z]
 
 proc parseScanner*(scannerString: string): Scanner =
   scannerString.strip.splitLines[1..^1].map(parsePos)
@@ -36,31 +61,15 @@ proc parseScanner*(scannerString: string): Scanner =
 proc parseScanners*(filename: string): Scanners =
   filename.readFile.split("\n\n").map(parseScanner)
 
-iterator rotations(): Rot =
-  var p = [1, 2, 3]
-  while true:
-    let (x, y, z) = (p[0], p[1], p[2])
-    yield (x, y, z)
-    yield (x, y, -z)
-    yield (x, -y, z)
-    yield (x, -y, -z)
-    yield (-x, y, z)
-    yield (-x, y, -z)
-    yield (-x, -y, z)
-    yield (-x, -y, -z)
-    if not p.nextPermutation:
-      break
-
-proc rotated(pos: Pos, r: Rot): Pos =
-  let p = [pos[0], pos[1], pos[2]]
-  (
+proc rotated(p: Pos, r: Rot): Pos =
+  [
     p[r[0].abs - 1] * r[0].sgn,
     p[r[1].abs - 1] * r[1].sgn,
     p[r[2].abs - 1] * r[2].sgn,
-  )
+  ]
 
 proc findRelativePosRot(a: Scanner, b: Scanner): (bool, Pos, Rot) =
-  for r in rotations():
+  for r in ROTATIONS:
     var diffs: CountTable[Pos]
     for p1 in a:
       for p2 in b:
@@ -69,7 +78,7 @@ proc findRelativePosRot(a: Scanner, b: Scanner): (bool, Pos, Rot) =
         if diffs[d] == MIN_BEACON_OVERLAP:
           return (true, d, r)
 
-  (false, (0,0,0), (0,0,0))
+  result[0] = false
 
 proc findPath(relations: seq[Table[int, (Pos, Rot)]], j: int, path: var seq[int]): bool =
   if j in path:
@@ -91,22 +100,26 @@ proc findPath(relations: seq[Table[int, (Pos, Rot)]], j: int, path: var seq[int]
 proc solution*(scanners: Scanners): (int, int) =
   let l = scanners.len
   var relations = newSeqWith(l, initTable[int, (Pos, Rot)]())
-  for i in 0..<l:
+  for i in 0..<l-1:
     let a = scanners[i]
-    for j in 1..<l:
-      if i == j:
-        continue
+    for j in i+1..<l:
       let b = scanners[j]
-      var (found, p, r) = findRelativePosRot(a, b)
+      let (found, p, r) = findRelativePosRot(a, b)
       if found:
         relations[j][i] = (p, r)
+        if i != 0:
+          var rr: Rot
+          rr[r[0].abs - 1] = 1 * r[0].sgn
+          rr[r[1].abs - 1] = 2 * r[1].sgn
+          rr[r[2].abs - 1] = 3 * r[2].sgn
+          relations[i][j] = ((-p).rotated(rr), rr)
 
   var paths = newSeqWith(l, newSeq[int]())
   for j in 1..<l:
     discard relations.findPath(j, paths[j])
 
   var s = scanners
-  var origins = newSeqWith[Pos](l, (0,0,0))
+  var origins = newSeqWith[Pos](l, [0,0,0])
   for j in 1..<l:
     let path = paths[j]
     for i in 0..path.len-2:
