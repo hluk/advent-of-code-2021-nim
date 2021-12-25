ebbdb38006eecbe96e694853cf32f790aeb85dce
Lukas Holecek <hluk@email.cz>
2021-12-25 08:43:26 +0100

    Refactor day 25

--- 25/main.nim
+++ 25/main.nim
@@ -1,6 +1,7 @@
 import sequtils
 import strutils
 import tables
+import zero_functional
 
 type
   Cucumber = enum East South
@@ -31,38 +32,43 @@ proc `$`(map: Map): string =
   for p, c in map.m:
     result[p.y * (map.w + 1) + p.x] = c.toChar
 
+proc toEast(p: Pos, map: Map): Pos =
+  ((p.x + 1) mod map.w, p.y)
+
+proc toSouth(p: Pos, map: Map): Pos =
+  (p.x, (p.y + 1) mod map.h)
+
+proc canMoveEast(p: Pos, map: Map): bool =
+  p.toEast(map) notin map.m
+
+proc canMoveSouth(p: Pos, map, map2: Map): bool =
+  let p2 = p.toSouth(map)
+  p2 notin map2.m and map.m.getOrDefault(p2) == East
+
 proc part1*(map: Map): int =
-  var moved = true
   var map = map
   var map2: Map
-  while moved:
-    moved = false
+  while true:
     map2 = Map(w: map.w, h: map.h)
 
-    #echo result
-    #echo map
+    let east = map.m.pairs --> (p, c) --> filter(c == East).partition(p.canMoveEast(map))
+    for (p, _) in east.yes:
+      map2.m[p.toEast(map)] = East
+    for (p, _) in east.no:
+      map2.m[p] = East
 
-    for p, c in map.m:
-      if c == East:
-        var p2 = ((p.x + 1) mod map.w, p.y)
-        if p2 notin map.m:
-          map2.m[p2] = c
-          moved = true
-        else:
-          map2.m[p] = c
-
-    for p, c in map.m:
-      if c == South:
-        var p2 = (p.x, (p.y + 1) mod map.h)
-        if p2 notin map2.m and (p2 notin map.m or map.m[p2] == East):
-          map2.m[p2] = c
-          moved = true
-        else:
-          map2.m[p] = c
+    let south = map.m.pairs --> (p, c) --> filter(c == South).partition(p.canMoveSouth(map, map2))
+    for (p, _) in south.yes:
+      map2.m[p.toSouth(map)] = South
+    for (p, _) in south.no:
+      map2.m[p] = South
 
     map.swap(map2)
 
     inc result
+
+    if east.yes.len == 0 and south.yes.len == 0:
+      break
 
 if isMainModule:
   let map = "input".parseMap
